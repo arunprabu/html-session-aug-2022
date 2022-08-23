d2390e3c561ebe1be309cc3a6bf20081636aba61
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-08-23 09:45:11 +0530

    updated

--- cognizant-v3-website/assets/scripts/main.js
+++ cognizant-v3-website/assets/scripts/main.js
@@ -2,3 +2,24 @@
 function onClickHandler(){
   alert('clicked');
 }
+
+document.getElementById('demoBtn')?.addEventListener('click', onClickHandler);
+
+function dragStartHandler(event){
+  console.log('dragging');
+  console.log(event.target.id);
+  event.dataTransfer.setData('draggedItem', event.target.id);
+}
+
+function dragOverHandler(event){
+  console.log(event);
+  // allowing to drop
+  event.preventDefault();
+}
+
+function dropHandler(event){
+  event.preventDefault();
+  var droppedItem = event.dataTransfer.getData('draggedItem');
+  console.log(droppedItem);
+  event.target.appendChild(document.getElementById(droppedItem));
+}
