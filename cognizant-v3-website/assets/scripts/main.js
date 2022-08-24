1c17ebb244f94acdbd7156af61c1df04c36fc45e
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-08-24 09:37:19 +0530

    updated

--- cognizant-v3-website/assets/scripts/main.js
+++ cognizant-v3-website/assets/scripts/main.js
@@ -23,3 +23,59 @@ function dropHandler(event){
   console.log(droppedItem);
   event.target.appendChild(document.getElementById(droppedItem));
 }
+
+/* Local Storage */
+function savePincodeHandler(){
+  var enteredPincode = document.getElementById('pincodeInput').value;
+  console.log(enteredPincode);
+  localStorage.setItem('pin', enteredPincode);
+  alert('saved in localStorage. Check browser console');
+}
+
+document.getElementById('savePincodeBtn')?.addEventListener('click', savePincodeHandler);
+
+function accessLocationHandler(){
+  console.log('accessing location.. pls wait');
+  navigator.geolocation.getCurrentPosition( locationAccessGranted, locationAccessDenied )
+}
+
+function locationAccessGranted(position){
+  console.log(position);
+  alert('You are here.. Latitude: ' + position.coords.latitude + ' and Longitude is: ' + position.coords.longitude)
+}
+
+function locationAccessDenied(){
+  alert('you denied to access the location');
+}
+
+document.getElementById('accessLocationBtn')?.addEventListener('click', accessLocationHandler);
+
+
+// Custom Elments 
+//1 
+class MyReport extends HTMLElement{
+  constructor(){
+    super();
+  }
+  
+  connectedCallBack(){
+    console.log('connectedCallBack');
+  }
+}
+
+customElements.define('my-report', MyReport);
+
+
+// 2.
+class MyButton extends HTMLButtonElement{
+  // custom element related methods 
+  constructor(){
+    super();
+    this.addEventListener('click', () => {
+      alert('clicked');
+    })
+  }
+}
+
+customElements.define('my-button', MyButton, { extends: 'button'});
+
